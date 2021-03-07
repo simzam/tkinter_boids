0b0806379952d71eb6d0c34a7e28f5e021e8d8b3
Simon Iversen <simon.iversen@protonmail.com>
2021-03-07 16:50:09 +0100

    Added methods for calculating position.

--- boid.py
+++ boid.py
@@ -1,10 +1,12 @@
-""" class for each individual boid """
+""" class for each individual boid.
+boids are described in http://www.red3d.com/cwr/boids/
+"""
+from math import sqrt
 
 class Boid:
-    def __init__(self, position, velocity, id_num):
+    def __init__(self, position, velocity):
         self.pos = position
         self.vel = velocity
-        self.id_num = id_num
 
 
     def update_velocity(self, v):
@@ -17,4 +19,35 @@ class Boid:
         """ moving boid using the predefined velocity. """
         self.pos = (self.pos[0] + self.vel[0],
                     self.pos[1] + self.vel[1])
+
+
+    def separation(self, neighbours):
         
+        
+    def alignment(self, neighbours):
+            
+    def cohesion(self, neighbours):
+        avg_pos = [0, 0]
+        for boid in neighbours:
+            
+
+            
+
+    def avoid_boundaries(self, domain):
+    """ boids live inside a limited domain, in order to keep the boids in the 
+    window ensure that the move in the opposite direction when they encounter
+    a wall. This overshadows all other movement."""
+    
+        
+        
+        
+    def distance_from_point(self, point):
+        """ distance from position of boid to a given point. """
+        assert isinstance(point, tuple), "A point is defined to be a tuple."
+        assert len(point) == 2, "A point has two values."
+
+        return sqrt((self.pos[0] - point[0])**2 +
+                    (self.pos[1] - point[1])**2)
+
+    
+    
