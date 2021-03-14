3c3f0d653e05a78485905d792104bd977d157fda
simon <Simon Iversen>
2021-03-14 17:03:15 +0100

    Cleaned up file.

--- flock.py
+++ flock.py
@@ -1,21 +1,20 @@
-""" class for dealing with a flock of boids. """
-
 from tkinter import Tk
 from boidsgui import BoidsApp
 from boid import Boid
+
 
 class Flock():
     """ class for holding all the boids. """
     def __init__(self, radius, domain):
         self.num_boids = 0
         self.boids = []
-        self.radius = radius # the distance deciding if two boids are neighbours
-        self.domain = domain # coordinates of the world of the boids
-
+        # the distance deciding if two boids are neighbours
+        self.radius = radius
+        # coordinates of the world of the boids
+        self.domain = domain
 
     def _get_boids(self):
         return [(boid.pos, boid.vel) for boid in self.boids]
-
 
     def _add_tuple(self, t1, t2):
         # assert isinstance(t1, tuple)
@@ -26,13 +25,12 @@ class Flock():
 
         return (t1[0] + t2[0], t1[1] + t2[1])
 
-
     def _div_tuple(self, t1, const):
         return (t1[0] / const, t1[1] / const)
 
-
     def _find_close_boids(self, curr_boid):
-        """ find all the boids with the "self.radius" distance from this boid. """
+        """find all the boids within the "self.radius" distance from this
+        boid."""
         neighs = []
         neighs_pos = (0, 0)
         neighs_vel = (0, 0)
@@ -51,32 +49,31 @@ class Flock():
 
         return neighs, neighs_pos, neighs_vel
 
-
     def update(self):
         """ update the velocity of the boids."""
         for boid in self.boids:
             if boid.avoid_neighbours(self.domain):
                 continue
 
-            neighbours, neighbours_pos, neighbours_vel  = self._find_close_boids(boid)
+            neighs, neighs_pos, neighs_vel = self._find_close_boids(boid)
 
-            boid.separation(neighbours)
-            boid.alignment(neighbours)
-            boid.cohesion(neighbours)
+            boid.separation(neighs)
+            boid.alignment(neighs)
+            boid.cohesion(neighs)
 
 
-    def add_boid(self, pos = (0,0), vel = (0,0)):
+    def add_boid(self, pos=(0, 0), vel=(0, 0)):
+        # should do check whether boid were sucessfully created.
         self.boids.append(Boid(pos, vel))
 
 
 if __name__ == '__main__':
-
     RADIUS = 15
-    SIZE   = 1000
+    SIZE = 1000
 
     # TODO: fix nonquadratic canvas
-    #HEIGHT = 1000
-    #WIDTH  = 1800
+    # HEIGHT = 1000
+    # WIDTH  = 1800
 
     def loop(app, boids):
         if app.running:
